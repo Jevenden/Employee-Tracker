774ce463469b181bc2b7fa2cfbc3ef29c0ee8379
Jevenden <102879070+Jevenden@users.noreply.github.com>
2022-07-31 01:41:37 -0400

    Not being used

--- assist/queries.js
+++
@@ -1,7 +0,0 @@
-// export default function dbQuery(sql) {
-//   return new Promise((resolve, reject) => {
-//     database.query(sql, (err, results) => {
-//       err ? reject(err) : resolve(results);
-//     });
-//   });
-// }
